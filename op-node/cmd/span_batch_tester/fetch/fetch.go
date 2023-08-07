6fe378d544460768ec28595d274d18bcb3af16fe
pcw109550 <pcw109550@gmail.com>
2023-08-07 11:00:49 +0900

    Disable error return

--- op-node/cmd/span_batch_tester/fetch/fetch.go
+++ op-node/cmd/span_batch_tester/fetch/fetch.go
@@ -50,28 +50,31 @@ func Batches(client *ethclient.Client, config Config) error {
 }
 
 // fetchBatchesPerBlock gets a block & creates batch.
-func fetchBatchesPerBlock(client *ethclient.Client, l2BlockNumber *big.Int, config Config) error {
+func fetchBatchesPerBlock(client *ethclient.Client, l2BlockNumber *big.Int, config Config) {
 	// l1Client is not used yet
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 	l2Block, err := client.BlockByNumber(ctx, l2BlockNumber)
 	if err != nil {
-		return err
+		log.Fatal(err)
+		return
 	}
 	fmt.Println("Fetched L2 block: ", l2BlockNumber)
 	batch, _, err := derive.BlockToBatch(l2Block)
 	if err != nil {
-		return err
+		log.Fatal(err)
+		return
 	}
 	filename := path.Join(config.OutDirectory, fmt.Sprintf("%d.json", l2BlockNumber.Uint64()))
 	file, err := os.Create(filename)
 	if err != nil {
-		return err
+		log.Fatal(err)
+		return
 	}
 	defer file.Close()
 	enc := json.NewEncoder(file)
 	if err := enc.Encode(batch); err != nil {
-		return err
+		log.Fatal(err)
+		return
 	}
-	return nil
 }
