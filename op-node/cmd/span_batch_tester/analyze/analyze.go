57decb845d82c6bcd89fd80c12a3571b1813f06a
pcw109550 <pcw109550@gmail.com>
2023-08-08 17:20:52 +0900

    Get channel id list

--- op-node/cmd/span_batch_tester/analyze/analyze.go
+++ op-node/cmd/span_batch_tester/analyze/analyze.go
@@ -1,6 +1,10 @@
 package analyze
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 type Config struct {
 	InChannelDirectory            string
@@ -8,6 +12,38 @@ type Config struct {
 	OutDirectory                  string
 }
 
+type Result struct {
+}
+
+// Load channel ids which can be analyzed
+func LoadChannelIDs(config Config) []string {
+	spanBatchFileNameSet := map[string]struct{}{}
+	spanBatchFiles, err := os.ReadDir(config.InSpanBatchDirectoryDirectory)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, spanBatchFile := range spanBatchFiles {
+		spanBatchFileNameSet[spanBatchFile.Name()] = struct{}{}
+	}
+	var out []string
+	channelFiles, err := os.ReadDir(config.InChannelDirectory)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, channelFile := range channelFiles {
+		_, ok := spanBatchFileNameSet[channelFile.Name()]
+		if ok {
+			out = append(out, channelFile.Name())
+			fmt.Println(channelFile.Name())
+		}
+	}
+	return out
+}
+
 func Analyze(config Config) {
-	fmt.Println("eeor")
+	if err := os.MkdirAll(config.OutDirectory, 0750); err != nil {
+		log.Fatal(err)
+	}
+	channelIDs := LoadChannelIDs(config)
+	_ = channelIDs
 }
