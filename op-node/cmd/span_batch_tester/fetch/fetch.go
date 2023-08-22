a36dc0e078b9af5825566d9b455d3780718a7fd8
pcw109550 <pcw109550@gmail.com>
2023-08-22 14:05:13 +0900

    Use errgroup

--- op-node/cmd/span_batch_tester/fetch/fetch.go
+++ op-node/cmd/span_batch_tester/fetch/fetch.go
@@ -8,11 +8,11 @@ import (
 	"math/big"
 	"os"
 	"path"
-	"sync"
 	"time"
 
 	"github.com/ethereum-optimism/optimism/op-node/rollup/derive"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"golang.org/x/sync/errgroup"
 )
 
 type Config struct {
@@ -28,53 +28,52 @@ func Batches(client *ethclient.Client, config Config) error {
 	if err := os.MkdirAll(config.OutDirectory, 0750); err != nil {
 		log.Fatal(err)
 	}
-	concurrentRequests := config.ConcurrentRequests
+	concurrentRequests := int(config.ConcurrentRequests)
 
-	var wg sync.WaitGroup
-	for i := config.Start; i < config.End; i += concurrentRequests {
-		end := i + concurrentRequests
-		if end > config.End {
-			end = config.End
+	g, ctx := errgroup.WithContext(context.Background())
+	g.SetLimit(concurrentRequests)
+
+	for i := config.Start; i < config.End; i++ {
+		if err := ctx.Err(); err != nil {
+			break
 		}
-		for j := i; j < end; j++ {
-			wg.Add(1)
-			number := new(big.Int).SetUint64(j)
-			go func() {
-				defer wg.Done()
-				fetchBatchesPerBlock(client, number, config)
-			}()
-		}
-		wg.Wait()
+		number := new(big.Int).SetUint64(i)
+		g.Go(func() error {
+			if err := fetchBatchesPerBlock(client, number, config); err != nil {
+				return fmt.Errorf("error occurred while fetching block %d: %w", i, err)
+			}
+			return nil
+		})
+	}
+	if err := g.Wait(); err != nil {
+		log.Fatal(err)
 	}
 	return nil
 }
 
 // fetchBatchesPerBlock gets a block & creates batch.
-func fetchBatchesPerBlock(client *ethclient.Client, l2BlockNumber *big.Int, config Config) {
+func fetchBatchesPerBlock(client *ethclient.Client, l2BlockNumber *big.Int, config Config) error {
 	// l1Client is not used yet
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 	l2Block, err := client.BlockByNumber(ctx, l2BlockNumber)
 	if err != nil {
-		log.Fatal(err)
-		return
+		return err
 	}
 	fmt.Println("Fetched L2 block: ", l2BlockNumber)
 	batch, _, err := derive.BlockToBatch(l2Block)
 	if err != nil {
-		log.Fatal(err)
-		return
+		return err
 	}
 	filename := path.Join(config.OutDirectory, fmt.Sprintf("%d.json", l2BlockNumber.Uint64()))
 	file, err := os.Create(filename)
 	if err != nil {
-		log.Fatal(err)
-		return
+		return err
 	}
 	defer file.Close()
 	enc := json.NewEncoder(file)
 	if err := enc.Encode(batch); err != nil {
-		log.Fatal(err)
-		return
+		return err
 	}
+	return nil
 }
