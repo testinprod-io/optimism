d6b4c34cf7dc8ab4389eb9e4abb5363da87bd399
pcw109550 <pcw109550@gmail.com>
2023-08-29 18:27:35 +0900

    Add tests for field permutation

--- op-node/rollup/derive/batch_tx_v3_test.go
+++ op-node/rollup/derive/batch_tx_v3_test.go
@@ -51,3 +51,29 @@ func TestRecoverV(t *testing.T) {
 
 	assert.Equal(t, originalVs, recoveredVs, "recovered v mismatch")
 }
+
+func TestTxFieldPermutation(t *testing.T) {
+	InitializePermutations()
+
+	rng := rand.New(rand.NewSource(0x4141414141414141))
+
+	K := 100
+	rand.Shuffle(len(BatchV2TxsV3FieldPerms), func(i, j int) {
+		BatchV2TxsV3FieldPerms[i], BatchV2TxsV3FieldPerms[j] = BatchV2TxsV3FieldPerms[j], BatchV2TxsV3FieldPerms[i]
+	})
+
+	// try marshaling with each permutations
+	// we do not permute every possiblity because it takes over 30s to do so
+	for _, perm := range BatchV2TxsV3FieldPerms[:K] {
+		// update global variable
+		BatchV2TxsV3FieldPerm = perm
+
+		batch := RandomBatchV2V1(rng)
+		enc, err := batch.MarshalBinary()
+		assert.NoError(t, err)
+		var dec BatchData
+		err = dec.UnmarshalBinary(enc)
+		assert.NoError(t, err)
+		assert.Equal(t, batch, &dec, "Batch not equal test case %v", perm)
+	}
+}
