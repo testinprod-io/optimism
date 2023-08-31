284bf0469d7f3f654d304d16b9ec087fa4d7a0c2
pcw109550 <pcw109550@gmail.com>
2023-08-31 22:21:46 +0900

    Save result

--- op-node/cmd/span_batch_tester/format/format.go
+++ op-node/cmd/span_batch_tester/format/format.go
@@ -3,6 +3,7 @@ package format
 import (
 	"bytes"
 	"compress/zlib"
+	"encoding/json"
 	"fmt"
 	"log"
 	"math/big"
@@ -19,6 +20,15 @@ type Config struct {
 	OutDirectory         string
 	ChainID              *big.Int
 	Permutation          []int
+}
+
+type Result struct {
+	TotalSpanBatchCount       int
+	ReducedSpanBatchCount     int
+	SizeDeltas                []int
+	SizeDeltaSum              int
+	OriginalCompressedSizes   []int
+	OriginalCompressedSizeSum int
 }
 
 func calcCompressedSize(data []byte) int {
@@ -62,6 +72,16 @@ func Compare(permutation *[]int, batchV2 convert.SpanBatchWithMetadata) (int, in
 	return delta, spanBatchCompressedSizeA
 }
 
+func writeResult(r Result, filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	enc := json.NewEncoder(file)
+	return enc.Encode(r)
+}
+
 func Format(config Config) {
 	// update global varibles. Weird but works
 	derive.ChainID = config.ChainID
@@ -80,20 +100,39 @@ func Format(config Config) {
 	cnt := 0
 	deltaSum := 0
 	originalCompressedSizeSum := 0
+	var originalCompressedSizes []int
+	var deltas []int
 	for i, spanBatchFile := range spanBatchFiles {
 		batchV2Filename := path.Join(config.InSpanBatchDirectory, spanBatchFile.Name())
-		fmt.Println(i, batchV2Filename)
-
 		// always reset perm to pass span batch hash check
 		derive.BatchV2TxsV3FieldPerm = []int{0, 1, 2, 3, 4, 5, 6}
 		batchV2 := analyze.LoadSpanBatch(batchV2Filename)
 		delta, originalCompressedSize := Compare(&config.Permutation, batchV2)
+		deltas = append(deltas, delta)
+		originalCompressedSizes = append(originalCompressedSizes, originalCompressedSize)
 		if delta >= 0 {
 			cnt++
 		}
 		deltaSum += delta
 		originalCompressedSizeSum += originalCompressedSize
-		fmt.Printf("[%d/%d] %d %d %d\n", cnt, len(spanBatchFiles), delta, deltaSum, originalCompressedSizeSum)
-		fmt.Println(100 * float64(deltaSum) / float64(originalCompressedSizeSum))
+		fmt.Printf("[%d/%d] cnt: %d, delta: %d, deltasum: %d, originalCompressedSizeSum: %d\n", i+1, len(spanBatchFiles), cnt, delta, deltaSum, originalCompressedSizeSum)
+		fmt.Printf("Reduction Percentage: %f %%\n", 100*float64(delta)/float64(originalCompressedSize))
+	}
+	result := Result{
+		TotalSpanBatchCount:       len(spanBatchFiles),
+		ReducedSpanBatchCount:     cnt,
+		SizeDeltas:                deltas,
+		SizeDeltaSum:              deltaSum,
+		OriginalCompressedSizes:   originalCompressedSizes,
+		OriginalCompressedSizeSum: originalCompressedSizeSum,
+	}
+	fmt.Printf("Final Reduction Percentage: %f %%\n", 100*float64(deltaSum)/float64(originalCompressedSizeSum))
+	permutationStr := ""
+	for _, v := range config.Permutation {
+		permutationStr += fmt.Sprintf("%d", v)
+	}
+	filename := path.Join(config.OutDirectory, fmt.Sprintf("%s.json", permutationStr))
+	if err := writeResult(result, filename); err != nil {
+		log.Fatal(err)
 	}
 }
