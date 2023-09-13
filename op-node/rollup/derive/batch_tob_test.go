dee555c9b9842f31a660fbc59263329e3764ddcd
pcw109550 <pcw109550@gmail.com>
2023-09-13 21:10:53 +0900

    Add batch fuzz tests

--- op-node/rollup/derive/batch_tob_test.go
+++ op-node/rollup/derive/batch_tob_test.go
@@ -1,9 +1,11 @@
 package derive
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/ethereum-optimism/optimism/op-node/testutils/fuzzerutils"
+	"github.com/ethereum/go-ethereum/rlp"
 	fuzz "github.com/google/gofuzz"
 	"github.com/stretchr/testify/require"
 )
@@ -31,5 +33,35 @@ func FuzzBatchRoundTrip(f *testing.F) {
 
 		// Ensure the round trip encoding of batch data did not result in data loss
 		require.Equal(t, &batchData, &dec, "round trip batch encoding/decoding did not match original values")
+	})
+}
+
+// FuzzBatchRoundTripRLP executes a fuzz test similar to TestBatchRoundTripRLP, which tests that arbitrary BatchData will be
+// encoded and decoded without loss of its original values.
+func FuzzBatchRoundTripRLP(f *testing.F) {
+	f.Fuzz(func(t *testing.T, fuzzedData []byte) {
+		// Create our fuzzer wrapper to generate complex values
+		typeProvider := fuzz.NewFromGoFuzz(fuzzedData).NilChance(0).MaxDepth(10000).NumElements(0, 0x100).AllowUnexportedFields(true)
+		fuzzerutils.AddFuzzerFunctions(typeProvider)
+
+		// Create our batch data from fuzzed data
+		var batchData BatchData
+		typeProvider.Fuzz(&batchData)
+
+		// Encode our batch data
+		var buf bytes.Buffer
+		err := batchData.EncodeRLP(&buf)
+		require.NoError(t, err)
+		enc := buf.Bytes()
+
+		// Decode our encoded batch data
+		var dec BatchData
+		r := bytes.NewReader(enc)
+		s := rlp.NewStream(r, 0)
+		err = dec.DecodeRLP(s)
+		require.NoError(t, err)
+
+		// Ensure the round trip encoding of batch data did not result in data loss
+		require.Equal(t, &batchData, &dec, "round trip batch RLP encoding/decoding did not match original values")
 	})
 }
